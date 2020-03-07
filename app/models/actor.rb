f03d2b7eda7390b5a3cea7a1ff2328c5a5a95aaa
Stela Capsa <stela.capsa@gmail.com>
2020-03-07 23:58:05 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
+  has_many :character
+  has_many :shows,    through: :character
   
+  
+  def full_name
+    "#{self.first_name}#{self.last_name}"
+  end 
+  
+  def list_roles
+   self.character.collect do |character|
+     "#{character.name} - #{character.show.name}"
+    end.join
+  end
 end
