0f436668a71d5397e6cb060d1de83214c8bf166a
Stela Capsa <stela.capsa@gmail.com>
2020-03-11 20:39:31 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,9 +3,9 @@ class Show < ActiveRecord::Base
   has_many :characters
 has_many :actors,    through: :characters
 
-def actors_list 
-actors.map do |n|
-  n.full_name
-end 
-end 
+   def actors_list 
+       actors.map do |n|
+       n.full_name
+   end 
+  end 
 end
